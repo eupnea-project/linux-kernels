7f587764194025fd4a43eec6384b484a63f07102
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-23 16:44:55 +0100

    Update version in build script too

--- .github/scripts/update-configs.py
+++ .github/scripts/update-configs.py
@@ -12,10 +12,10 @@ def bash(command: str) -> str:
 
 def update_stable(releases_json: dict) -> None:
     # Get the latest stable version
-    latest_stable = "v" + releases_json["latest_stable"]["version"]
+    latest_version = "v" + releases_json["latest_stable"]["version"]
 
     # Git clone the latest stable version
-    bash(f"git clone --depth=1 --branch={latest_stable} https://git.kernel.org/pub/scm/linux/kernel/git/"
+    bash(f"git clone --depth=1 --branch={latest_version} https://git.kernel.org/pub/scm/linux/kernel/git/"
          f"stable/linux.git")
 
     # Copy old config into local stable repo
@@ -24,16 +24,23 @@ def update_stable(releases_json: dict) -> None:
     # Update config
     bash("cd linux && make olddefconfig")
 
+    # Update bash script
+    with open("build.sh", "r") as file:
+        build_script = file.readlines()
+    build_script[1] = f"  KERNEL_VERSION={latest_version}"
+    with open("build.sh", "w") as file:
+        file.writelines(build_script)
+
     # Copy new config back to repo
     bash("cp linux/.config config-stable")
 
 
 def update_testing(releases_json: dict) -> None:
     # Get the latest mainline version
-    latest_stable = "v" + releases_json["releases"][0]["version"]
+    latest_version = "v" + releases_json["releases"][0]["version"]
 
     # Git clone the latest mainline version
-    bash(f"git clone --depth=1 --branch={latest_stable} https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/"
+    bash(f"git clone --depth=1 --branch={latest_version} https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/"
          f"linux.git")
 
     # Copy old config into local stable repo
@@ -41,6 +48,13 @@ def update_testing(releases_json: dict) -> None:
 
     # Update config
     bash("cd linux && make olddefconfig")
+
+    # Update bash script
+    with open("build.sh", "r") as file:
+        build_script = file.readlines()
+    build_script[1] = f"  KERNEL_VERSION={latest_version}"
+    with open("build.sh", "w") as file:
+        file.writelines(build_script)
 
     # Copy new config back to repo
     bash("cp linux/.config config-testing")

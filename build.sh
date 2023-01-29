fe87f7b443453e8ea50871a2f520083db7239539
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-01-29 21:05:49 +0100

    Fix missing variable

--- build.sh
+++ build.sh
@@ -79,7 +79,7 @@ echo "Modules archive created!"
 cd ../
 rm -r hdr || true
 mkdir -p hdr
-KVER=$(file -bL ../$VMLINUZ | grep -o 'version [^ ]*' | cut -d ' ' -f 2)
+KVER=$(file -bL ../bzImage | grep -o 'version [^ ]*' | cut -d ' ' -f 2)
 HDR_PATH=$(pwd)/hdr/linux-headers-$KVER
 
 # Build files
